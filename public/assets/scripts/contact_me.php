0696e5f8606882df718310a96c04173b19ad67bd
Fidel Hernandez <fidel.hdz@me.com>
2024-06-04 22:53:01 -0600

    Update

--- public/assets/scripts/contact_me.php
+++ public/assets/scripts/contact_me.php
@@ -1,33 +1,81 @@
 <?php
-if ($_POST) {
-	$to_Email   	= "fh@bicho.tech"; //Replace with recipient email address
-	$subject        = 'Mensaje enviado desde '.$_SERVER['SERVER_NAME']; //Subject line for emails
-	
-	//Sanitize input data using PHP filter_var().
-	$user_Email       = filter_var($_POST["correo"], FILTER_SANITIZE_EMAIL);
-	$user_Message     = filter_var($_POST["mensaje"], FILTER_SANITIZE_STRING);
-	$user_Name        = filter_var($_POST["nombre"], FILTER_SANITIZE_STRING);
-	$user_biz		  = filter_var($_POST["negocio"], FILTER_SANITIZE_STRING);
-	$user_services	  = filter_var($_POST["servicios"], FILTER_SANITIZE_STRING);
-	
-	$user_Message = str_replace("\&#39;", "'", $user_Message);
-	$user_Message = str_replace("&#39;", "'", $user_Message);
+	$data = json_decode(file_get_contents('php://input'), true);
+	//Import PHPMailer classes into the global namespace
+	//These must be at the top of your script, not inside a function
+	use phpmailer\phpmailer\PHPMailer;
+	use phpmailer\phpmailer\SMTP;
+	use phpmailer\phpmailer\Exception;
 
-	$body = $user_Message . "\r\n\n"  .'-- '.$user_Name. "\r\n" .'-- '.$user_Email. "\r\n" .'-- '.$user_biz. "\r\n" .'-- '.$user_services;
-	
-	//proceed with PHP email.
-	$headers = 'From: '.$user_Email.'' . "\r\n" .
-	'Reply-To: no-reply@bicho.tech' . "\r\n" .
-	'X-Mailer: PHP/' . phpversion();
-	
-	$sentMail = mail($to_Email, $subject, $body, $headers);
+	require __DIR__ . '/vendor/phpmailer/phpmailer/src/Exception.php';
+	require __DIR__ . '/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+	require __DIR__ . '/vendor/phpmailer/phpmailer/src/SMTP.php';
+	require_once(__DIR__ . '/vars/define_vars.php');
 
-	echo $sentMail;
-	
-	if(!$sentMail) {
-		echo 'No se pudo enviar la informacíon, favor de intentar más tarde.';
+	//Create an instance; passing `true` enables exceptions
+	$mail = new PHPMailer(true);
+
+	$form_Name      = $data['nombre'];
+	$form_Email     = $data['correo'];
+	$form_Company   = $data['negocio'];
+	$form_Services  = $data['servicios'];
+	$form_Message   = $data['mensaje'];
+
+	$body = "---------- <br>\r\n\n";
+	$body .= 'Nombre: ' . $form_Name . "<br>\r\n\n";
+	if ( $form_Email != '' ) {
+		$body .= 'Email: ' . $form_Email . "<br>\r\n";
 	} else {
-		echo 'Si ser envió.' . $body;
+		$form_Email = 'hi@bicho.tech';
 	}
-}
+	if ( $form_Company != '' ) $body .= 'Negocio: ' . $form_Company . "<br>\r\n";
+	if ( $form_Services != '' ) $body .= 'Servicio: ' . $form_Services . "<br>\r\n";
+	if ( $form_Message != '' ) $body .= 'Mensaje: ' . "\r\n" . $form_Message . "<br>\r\n";
+	$body .= "---------- <br>\r\n\n";
+
+	$body_alt = "---------- \r\n\n";
+	$body_alt .= 'Nombre: ' . $form_Name . "\r\n\n";
+	if ( $form_Email != '' ) {
+		$body_alt .= 'Email: ' . $form_Email . "\r\n";
+	} else {
+		$form_Email = 'hi@bicho.tech';
+	}
+	if ( $form_Company != '' ) $body_alt .= 'Compañía: ' . $form_Company . "\r\n";
+	if ( $form_Services != '' ) $body_alt .= 'Servicio: ' . $form_Services . "\r\n";
+	if ( $form_Message != '' ) $body_alt .= 'Mensaje: ' . "\r\n" . $form_Message . "\r\n";
+	$body_alt .= "---------- \r\n\n";
+
+	try {
+		//Server settings
+		$mail->CharSet    = "UTF-8";
+		$mail->Encoding   = 'base64';
+		$mail->SMTPDebug  = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+		$mail->isSMTP();                                            //Send using SMTP
+		$mail->Host       = MAIL_SMTPSERVER;	            //Set the SMTP server to send through
+		$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+		$mail->Username   = MAIL_USERNAME;	                //SMTP username
+		$mail->Password   = MAIL_PASSWORD;                  //SMTP password
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+		$mail->Port       = MAIL_SMTPPORT;                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+		//Recipients
+		$mail->setFrom(MAIL_PRINCIPAL);
+		$mail->addAddress(MAIL_PRINCIPAL, 'BichoTech');     //Add a recipient
+		$mail->addReplyTo(MAIL_PRINCIPAL, 'Información');
+		$mail->addBCC(MAIL_SECONDARY, 'Departamento de Ventas');
+
+		//Attachments
+		// $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+		// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+		//Content
+		$mail->isHTML(true);                                  //Set email format to HTML
+		$mail->Subject = 'Mensaje para BichoTech';
+		$mail->Body    = $body;
+		$mail->AltBody = $body_alt;
+
+		$mail->send();
+		echo 'Mensaje enviado exitosamente.';
+	} catch (Exception $e) {
+		echo "No se pudo enviar el mensaje.";
+	}
 ?>
---
+++ public/assets/scripts/vars/define_vars.php
@@ -0,0 +1,8 @@
+<?php
+    define("MAIL_SMTPSERVER", "smtp.hostinger.com");
+    define("MAIL_SMTPPORT", 465);
+    define("MAIL_USERNAME", "hi@bicho.tech");
+    define("MAIL_PASSWORD", "Piratex@30");
+    define("MAIL_PRINCIPAL", "hi@bicho.tech");
+    define("MAIL_SECONDARY", "eaguirre@bicho.tech");
+?>
