269c1fdef4d50accf972d7169a94094188cee104
Fidel Hernandez <fidel.hdz@me.com>
2024-07-09 14:06:33 -0600

    Open Graph Meta Tags added

--- public/assets/scripts/vars/define_vars.php
+++ public/assets/scripts/vars/define_vars.php
@@ -1,8 +1,8 @@
 <?php
     define("MAIL_SMTPSERVER", "smtp.hostinger.com");
     define("MAIL_SMTPPORT", 465);
-    define("MAIL_USERNAME", "hi@bicho.tech");
+    define("MAIL_USERNAME", "contacto@bicho.tech");
     define("MAIL_PASSWORD", "Piratex@30");
-    define("MAIL_PRINCIPAL", "hi@bicho.tech");
-    define("MAIL_SECONDARY", "eaguirre@bicho.tech");
+    define("MAIL_PRINCIPAL", "contacto@bicho.tech");
+    define("MAIL_SECONDARY", "fh@bicho.tech");
 ?>
